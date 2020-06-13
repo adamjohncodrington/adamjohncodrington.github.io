2d6c0c4e4c323ef67c769f6bb945cb6b0a2519b9
Adam Codrington <adamcodrington@adams-mbp.home>
2020-06-13 23:10:44 +0100

    chore: implement global style, ditch index css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+import Routes from "./routes";
+import { THEME, GlobalStyle } from "./styles";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={THEME}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
