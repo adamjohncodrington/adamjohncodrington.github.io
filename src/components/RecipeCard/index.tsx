b0c92127a0e2f2c563ec087e112125639d62b48e
Adam Codrington <adamcodrington@adams-mbp.home>
2020-06-17 08:26:11 +0100

    refactor: proper list elements

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-import { RecipeCardCost } from "./RecipeCardCost";
+import { MEASUREMENTS, NO_UNIT_COST_FOR_RECIPE_EXISTS } from "../../constants";
+import { getServeWithListItem, calculateRecipeCost } from "../../utils/global";
+
+import { CentredOnPhone } from "../CentredOnPhone";
+import { UnorderedList } from "../UnorderedList";
+import { VisibilityToggle } from "../VisibilityToggle";
 import { RecipeCardHeader } from "./RecipeCardHeader";
 import { RecipeCardIngredients } from "./RecipeCardIngredients";
-import { RecipeCardMethod } from "./RecipeCardMethod";
-import { RecipeCardImage } from "./RecipeCardImage";
-import { RecipeCardServeWith } from "./RecipeCardServeWith";
-import { VisibilityToggle } from "../VisibilityToggle";
 
+const StyledImage = styled.img(
+  ({
+    theme: {
+      recipe: {
+        image: { size }
+      }
+    }
+  }: ThemeProps) => css`
+    height: ${size};
+    width: ${size};
+  `
+);
+
+//TODO: move these px values to theme
 const RecipeBody = styled.div`
   padding-top: 5px;
 
   > * {
     padding: 10px 0 5px;
   }
+`;
+
+const UnitCostsContainer = styled.div`
+  font-size: 80%;
 `;
 
 export const RecipeCard: React.FC<IRecipeCard> = ({
@@ -31,6 +50,12 @@ export const RecipeCard: React.FC<IRecipeCard> = ({
 }) => {
   if (hide) return null;
 
+  const { displayCost, displayUnitCost } = calculateRecipeCost({
+    ingredients,
+    yieldQuantity: makes && makes.quantity,
+    recipeTitle: title
+  });
+
   return (
     <VisibilityToggle
       expandedAutomatically={newRecipe}
@@ -40,10 +65,38 @@ export const RecipeCard: React.FC<IRecipeCard> = ({
     >
       <RecipeBody>
         <RecipeCardIngredients makes={makes} ingredients={ingredients} />
-        {serveWith && <RecipeCardServeWith serveWith={serveWith} />}
-        {method && <RecipeCardMethod method={method} />}
-        {image && <RecipeCardImage imageSource={image} />}
-        <RecipeCardCost ingredients={ingredients} makes={makes} title={title} />
+
+        {serveWith && (
+          <UnorderedList
+            title="serve with"
+            items={serveWith.map((item: Array<IRecipeIngredient>) =>
+              getServeWithListItem(item)
+            )}
+          />
+        )}
+
+        {method && <UnorderedList title="method" items={method} showBullets />}
+
+        {image && (
+          <CentredOnPhone>
+            <StyledImage src={image} />
+          </CentredOnPhone>
+        )}
+
+        <CentredOnPhone>
+          <div>
+            approx. <strong>{displayCost}</strong> to make
+            {displayUnitCost !== NO_UNIT_COST_FOR_RECIPE_EXISTS &&
+              makes &&
+              makes.measurement &&
+              makes.quantity > 1 &&
+              makes.measurement !== MEASUREMENTS.GRAM && (
+                <UnitCostsContainer>
+                  <strong>{displayUnitCost}</strong> per {makes.measurement}
+                </UnitCostsContainer>
+              )}
+          </div>
+        </CentredOnPhone>
       </RecipeBody>
     </VisibilityToggle>
   );
