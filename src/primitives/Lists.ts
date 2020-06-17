4d200304417b577e877685fc4a25a5b4d325bb51
Adam Codrington <adamcodrington@adams-mbp.home>
2020-06-17 08:25:16 +0100

    refactor: minor

--- src/primitives/Lists.ts
+++ src/primitives/Lists.ts
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 
 export const Ul = styled.ul`
-  font-weight: bold;
   margin: 0;
   padding: 0;
-`;
-
-export const Li = styled.li`
-  font-weight: normal;
 `;
