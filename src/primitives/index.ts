a6fee670ee25c11fa0e3c160ba559b9d3bf5f637
Adam Codrington <adamcodrington@adams-mbp.home>
2020-06-17 08:25:32 +0100

    chore: remove obsolete li primitive

--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -1,6 +1,6 @@
 export { Circle } from "./Circle";
 export { FlexRow, FlexColumn } from "./Flex";
 export { Headers } from "./Headers";
-export { Ul, Li } from "./Lists";
+export { Ul } from "./Lists";
 export { SvgIcon } from "./SvgIcon";
 export { Text } from "./Text";
