37224c4d0d1e83a582de961ad38e02115f3f4e96
Adam Codrington <adamcodrington@Adams-MacBook-Pro.local>
2020-06-22 10:11:44 +0100

    refactor: move portions of data to pages

--- src/pages/theatre.ts
+++ src/pages/theatre.ts
@@ -1,35 +1,75 @@
-import { EVENT_CARD_TYPES, PAGE_SECTIONS } from "CONSTANTS";
+import {
+  ACTORS,
+  EVENT_CARD_TYPES,
+  FRIENDS,
+  PLAYS,
+  PAGE_SECTIONS,
+  THEATRE_VENUES
+} from "CONSTANTS";
 import { DATA_THEATRE } from "data";
-import { mapToEventCardData, mapYearsToEventCardPageSections } from "factories";
+import {
+  mapToEventCardData,
+  mapYearsToEventCardPageSections,
+  mapToCountedList
+} from "factories";
 import { ICONS } from "resources";
+import { isInFuture } from "utils";
+
+const { ALL, ALL_GROUPED_BY_YEAR } = DATA_THEATRE;
 
 const eventCardType = EVENT_CARD_TYPES.THEATRE;
 const mapTheatreVisitToEventCards = (parms: any) =>
   mapToEventCardData({ ...parms, eventCardType });
 
-const { ALL_GROUPED_BY_YEAR, UPCOMING } = DATA_THEATRE;
+const FAVOURITES: Array<ITheatreData> = ALL.filter(item => item.favourite);
+const UPCOMING = ALL.filter(theatreTrip => isInFuture(theatreTrip.dates));
+
+const mapToCountedListWrapper = (parms: any) =>
+  mapToCountedList({
+    ...parms,
+    allData: ALL,
+    favouritedData: FAVOURITES
+  });
+
+const actors: any = mapToCountedListWrapper({
+  items: ACTORS,
+  pageSectionTitle: PAGE_SECTIONS.ACTOR
+});
+const friends: any = mapToCountedListWrapper({
+  items: FRIENDS,
+  pageSectionTitle: PAGE_SECTIONS.FRIEND,
+  filter: "theatre"
+});
+const plays: any = mapToCountedListWrapper({
+  items: PLAYS,
+  pageSectionTitle: PAGE_SECTIONS.PLAY
+});
+const venues: any = mapToCountedListWrapper({
+  items: THEATRE_VENUES,
+  pageSectionTitle: PAGE_SECTIONS.THEATRE_VENUES
+});
 
 const SECTIONS_STATS: Array<IPageSection> = [
   {
     details: PAGE_SECTIONS.ACTOR,
     icon: ICONS.ARTIST,
-    data: DATA_THEATRE.ACTORS
+    data: actors
   },
   {
     details: PAGE_SECTIONS.FRIEND,
     icon: ICONS.PEOPLE,
-    data: DATA_THEATRE.FRIENDS
+    data: friends
   },
   {
     details: PAGE_SECTIONS.PLAY,
     icon: ICONS.BOOK,
-    data: DATA_THEATRE.PLAYS,
+    data: plays,
     showSectionLength: true
   },
   {
     details: PAGE_SECTIONS.THEATRE_VENUES,
     icon: ICONS.THEATRE,
-    data: DATA_THEATRE.VENUES,
+    data: venues,
     showSectionLength: true
   },
   {
