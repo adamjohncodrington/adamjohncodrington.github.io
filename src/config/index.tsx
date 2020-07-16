40b30ff28cf825b374ffa8ee113ccab9218acbfc
Adam Codrington <adamcodrington@adams-mbp.home>
2020-07-16 20:33:47 +0100

    wip

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -7,5 +7,3 @@ export const SHOW_EVENT_CARD_PHOTOS: boolean = false;
 export const USE_HOVER_STYLING: boolean = false;
 
 export const SPEED_UP_PAGE_LOAD: boolean = true;
-
-export const USE_REGULAR_HEART: boolean = true;
