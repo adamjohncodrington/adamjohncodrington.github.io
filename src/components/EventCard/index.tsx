ecf72973bd7e5817a9a22d286b80b68e0bc85024
Adam Codrington <adamcodrington@adams-mbp.home>
2020-07-20 13:10:04 +0100

    refactor: minor

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { useThemeContext } from "context";
 import { GridColumnDiv } from "primitives";
 
 import { Disclosure } from "../Disclosure";
@@ -33,80 +32,72 @@ export const EventCard: React.FC<IEventCard> = ({
   hidePhotosIcon,
   hideSecondaryBody,
   hideSubtitle
-}) => {
-  const {
-    eventCard: { companySymbol }
-  }: ITheme = useThemeContext();
+}) => (
+  <>
+    <Disclosure
+      isStatic={!(youtubeVideo || (photos && photos.length > 0))}
+      Header={() => (
+        <GridColumnDiv
+          data-test="event-card-grid-container"
+          equalWidthColumns={false}
+        >
+          <EventCardTextContainer data-test="event-card-container">
+            <CardTitleBold data-test="event-card-title" favourite={favourite}>
+              {title}
+            </CardTitleBold>
 
-  return (
-    <>
-      <Disclosure
-        isStatic={!(youtubeVideo || (photos && photos.length > 0))}
-        Header={() => (
-          <GridColumnDiv
-            data-test="event-card-grid-container"
-            equalWidthColumns={false}
-          >
-            <EventCardTextContainer data-test="event-card-container">
-              <CardTitleBold data-test="event-card-title" favourite={favourite}>
-                {title}
-              </CardTitleBold>
-
-              {subtitle && !hideSubtitle && (
-                <CardSubtitleBold data-test="event-card-subtitle">
-                  {subtitle}
-                </CardSubtitleBold>
-              )}
-
-              {body && <span data-test="event-card-body">{body}</span>}
-
-              {secondaryBody && !hideSecondaryBody && (
-                <EventCardSecondaryBody data-test="event-card-secondary-body">
-                  {secondaryBody}
-                </EventCardSecondaryBody>
-              )}
-
-              {countdownText && (
-                <>
-                  <EventCardCountdown data-test="event-card-countdown">
-                    {countdownText}
-                  </EventCardCountdown>
-
-                  {note && (
-                    <EventCardNote data-test="event-card-note">{`(${note})`}</EventCardNote>
-                  )}
-                </>
-              )}
-            </EventCardTextContainer>
-
-            {(company.length > 0 || youtubeVideo) && (
-              <EventCardSymbolsContainer>
-                {!hideCompany &&
-                  company
-                    .sort((a: IFriend, b: IFriend) =>
-                      a.initials > b.initials ? 1 : -1
-                    )
-                    .map(({ initials }: IFriend, index: number) => (
-                      <RoundedSymbol key={index} type="friend">
-                        {initials}
-                      </RoundedSymbol>
-                    ))}
-
-                {youtubeVideo && !hideVideoIcon && (
-                  <RoundedSymbol type="video" />
-                )}
-                {photos && !hidePhotosIcon && <RoundedSymbol type="photo" />}
-              </EventCardSymbolsContainer>
+            {subtitle && !hideSubtitle && (
+              <CardSubtitleBold data-test="event-card-subtitle">
+                {subtitle}
+              </CardSubtitleBold>
             )}
-          </GridColumnDiv>
-        )}
-        Panel={
-          <>
-            {youtubeVideo && <YouTubeVideo {...youtubeVideo} />}
-            {photos && <PhotoGrid photos={photos} columnCount={2} />}
-          </>
-        }
-      />
-    </>
-  );
-};
+
+            {body && <span data-test="event-card-body">{body}</span>}
+
+            {secondaryBody && !hideSecondaryBody && (
+              <EventCardSecondaryBody data-test="event-card-secondary-body">
+                {secondaryBody}
+              </EventCardSecondaryBody>
+            )}
+
+            {countdownText && (
+              <>
+                <EventCardCountdown data-test="event-card-countdown">
+                  {countdownText}
+                </EventCardCountdown>
+
+                {note && (
+                  <EventCardNote data-test="event-card-note">{`(${note})`}</EventCardNote>
+                )}
+              </>
+            )}
+          </EventCardTextContainer>
+
+          {(company.length > 0 || youtubeVideo) && (
+            <EventCardSymbolsContainer>
+              {!hideCompany &&
+                company
+                  .sort((a: IFriend, b: IFriend) =>
+                    a.initials > b.initials ? 1 : -1
+                  )
+                  .map(({ initials }: IFriend, index: number) => (
+                    <RoundedSymbol key={index} type="friend">
+                      {initials}
+                    </RoundedSymbol>
+                  ))}
+
+              {youtubeVideo && !hideVideoIcon && <RoundedSymbol type="video" />}
+              {photos && !hidePhotosIcon && <RoundedSymbol type="photo" />}
+            </EventCardSymbolsContainer>
+          )}
+        </GridColumnDiv>
+      )}
+      Panel={
+        <>
+          {youtubeVideo && <YouTubeVideo {...youtubeVideo} />}
+          {photos && <PhotoGrid photos={photos} columnCount={2} />}
+        </>
+      }
+    />
+  </>
+);
