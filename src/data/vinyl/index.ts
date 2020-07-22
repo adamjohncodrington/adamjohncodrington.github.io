40936611ec1f015cd44c1f96337e6b3404bd5ef1
Adam Codrington <adamcodrington@adams-mbp.home>
2020-07-22 22:07:16 +0100

    refactor wip

--- src/data/vinyl/index.ts
+++ src/data/vinyl/index.ts
@@ -1,23 +1,13 @@
 import * as VINYLS_2005_2008 from "./vinyls-2005-2008";
 import * as VINYLS_2009_2012 from "./vinyls-2009-2012";
-import * as VINYLS_2013 from "./vinyls-2013";
-import * as VINYLS_2014 from "./vinyls-2014";
-import * as VINYLS_2015 from "./vinyls-2015";
-import * as VINYLS_2016 from "./vinyls-2016";
-import * as VINYLS_2017 from "./vinyls-2017";
-import * as VINYLS_2018 from "./vinyls-2018";
-import * as VINYLS_2019 from "./vinyls-2019";
-import * as VINYLS_2020 from "./vinyls-2020";
+import * as VINYLS_2013_2016 from "./vinyls-2013-2016";
+import * as VINYLS_2017_2019 from "./vinyls-2017-2019";
+import * as VINYLS_2020_2022 from "./vinyls-2020-2022";
 
 export const VINYLS: Array<IVinyl> = Object.values({
   ...VINYLS_2005_2008,
   ...VINYLS_2009_2012,
-  ...VINYLS_2013,
-  ...VINYLS_2014,
-  ...VINYLS_2015,
-  ...VINYLS_2016,
-  ...VINYLS_2017,
-  ...VINYLS_2018,
-  ...VINYLS_2019,
-  ...VINYLS_2020
+  ...VINYLS_2013_2016,
+  ...VINYLS_2017_2019,
+  ...VINYLS_2020_2022
 });
---
+++ src/data/vinyl/vinyls-2017-2019.ts
@@ -0,0 +1,126 @@
+import { PHOTOS } from "resources";
+import { MUSICIANS } from "@constants";
+
+export const MUNA_ABOUT_U: IVinyl = {
+  musician: MUSICIANS.MUNA,
+  title: "About U",
+  artwork: PHOTOS.ALBUMS.MUNA_ABOUT_U,
+  appleMusicId: "about-u/1171832662",
+  year: 2017,
+  purchased: true
+};
+
+export const PVRIS_AWKOHAWNOH: IVinyl = {
+  musician: MUSICIANS.PVRIS,
+  title: "All We Know of Heaven, All We Need of Hell",
+  artwork: PHOTOS.ALBUMS.PVRIS_AWKOHAWNOH,
+  appleMusicId: "all-we-know-of-heaven-all-we-need-of-hell/1227528237",
+  year: 2017,
+  purchased: true,
+  signed: true
+};
+
+export const GEORGE_FITZGERALD_ALL_THAT_MUST_BE: IVinyl = {
+  musician: MUSICIANS.GEORGE_FITZGERALD,
+  title: "All That Must Be",
+  artwork: PHOTOS.ALBUMS.GEORGE_FITZGERALD_ALL_THAT_MUST_BE,
+  appleMusicId: "all-that-must-be/1322664114",
+  year: 2018
+};
+
+export const JON_HOPKINS_SINGULARITY: IVinyl = {
+  musician: MUSICIANS.JON_HOPKINS,
+  title: "Singularity",
+  artwork: PHOTOS.ALBUMS.JON_HOPKINS_SINGULARITY,
+  appleMusicId: "singularity/1349736537",
+  year: 2018
+};
+
+export const LANY_MALIBU_NIGHTS: IVinyl = {
+  musician: MUSICIANS.LANY,
+  title: "Malibu Nights",
+  artwork: PHOTOS.ALBUMS.LANY_MALIBU_NIGHTS,
+  appleMusicId: "malibu-nights/1435482172",
+  year: 2018
+};
+
+export const SOPHIE_OOEPUI: IVinyl = {
+  musician: MUSICIANS.SOPHIE,
+  title: "Oil of Every Pearl's Un-Insides",
+  artwork: PHOTOS.ALBUMS.SOPHIE_OOEPUI,
+  appleMusicId: "oil-of-every-pearls-un-insides/1395045797",
+  year: 2018
+};
+
+export const _1975_BRIEF_INQUIRY: IVinyl = {
+  musician: MUSICIANS._1975_THE,
+  title: "A Brief Inquiry Into Online Relationships",
+  artwork: PHOTOS.ALBUMS._1975_BRIEF_INQUIRY,
+  appleMusicId: "a-brief-inquiry-into-online-relationships/1435546528",
+  year: 2018,
+  purchased: true
+};
+
+export const ALLIE_X_SUPER_SUNSET: IVinyl = {
+  musician: MUSICIANS.ALLIE_X,
+  title: "Super Sunset",
+  artwork: PHOTOS.ALBUMS.ALLIE_X_SUPER_SUNSET,
+  appleMusicId: "super-sunset/1421851140",
+  year: 2018,
+  purchased: true,
+  signed: true
+};
+
+export const ARIANA_GRANDE_TUN: IVinyl = {
+  musician: MUSICIANS.ARIANA_GRANDE,
+  title: "thank u, next",
+  artwork: PHOTOS.ALBUMS.ArianaGrande_TUN,
+  appleMusicId: "thank-u-next/1450330588",
+  year: 2019
+};
+
+export const BANKS_III: IVinyl = {
+  musician: MUSICIANS.BANKS,
+  appleMusicId: "iii/1467547466",
+  title: "III",
+  artwork: PHOTOS.ALBUMS.BANKS_III,
+  year: 2019,
+  purchased: true,
+  signed: true
+};
+
+export const LEON_LEON: IVinyl = {
+  musician: MUSICIANS.LEON,
+  title: MUSICIANS.LEON.name,
+  appleMusicId: "l%C3%A9on/1450257286",
+  year: 2019,
+  artwork: PHOTOS.ALBUMS.LEON_LEON,
+  purchased: true
+};
+
+export const FKA_TWIGS_MAGDALENE: IVinyl = {
+  musician: MUSICIANS.FKA_TWIGS,
+  title: "Magdalene",
+  appleMusicId: "magdalene/1477652618",
+  year: 2019,
+  artwork: PHOTOS.ALBUMS.FKA_TWIGS_MAGDALENE,
+  purchased: true
+};
+
+export const CAROLINE_POLACHEK_PANG: IVinyl = {
+  musician: MUSICIANS.CAROLINE_POLACHEK,
+  title: "Pang",
+  artwork: PHOTOS.ALBUMS.CAROLINE_POLACHEK_PANG,
+  appleMusicId: "pang/1482421639",
+  year: 2019,
+  purchased: true
+};
+
+export const KIM_PETRAS_CLARITY: IVinyl = {
+  musician: MUSICIANS.KIM_PETRAS,
+  title: "Clarity",
+  artwork: PHOTOS.ALBUMS.KIM_PETRAS_CLARITY,
+  year: 2019,
+  appleMusicId: "clarity/1469298192",
+  purchased: true
+};
