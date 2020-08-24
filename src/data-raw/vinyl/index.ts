c783bd24cd3f82cc8ae83b9dcb17cf36b4de0bff
Adam Codrington <adamcodrington@adams-mbp.home>
2020-08-24 14:17:29 +0100

    refactor purchased vinyls

--- src/data-raw/vinyl/index.ts
+++ src/data-raw/vinyl/index.ts
@@ -1,11 +1,11 @@
-import * as collection from "./collection";
-import * as ordered from "./ordered";
+import * as purchased from "./purchased";
 import * as wish_list from "./wish-list";
 
-export const VINYL_COLLECTION: IVinyl[] = Object.values(collection);
-export const VINYL_ORDERED: IVinyl[] = Object.values(ordered);
-export const VINYL_PURCHASED: IVinyl[] = Object.values({
-  ...collection,
-  ...ordered
-});
+export const VINYL_PURCHASED: IVinyl[] = Object.values(purchased);
+export const VINYL_COLLECTION: IVinyl[] = VINYL_PURCHASED.filter(
+  ({ arrived }: IVinyl): boolean => arrived
+);
+export const VINYL_ORDERED: IVinyl[] = VINYL_PURCHASED.filter(
+  ({ arrived }: IVinyl): boolean => !arrived
+);
 export const VINYL_WISH_LIST: IAlbum[] = Object.values(wish_list);
--- src/data-raw/vinyl/purchased.ts
+++ src/data-raw/vinyl/purchased.ts
@@ -14,8 +14,70 @@ const {
   MUSICIAN_WEB_STORE,
   ROUGH_TRADE_EAST,
   ROUGH_TRADE_WEST,
-  ROUGH_TRADE_NOTTINGHAM
+  ROUGH_TRADE_NOTTINGHAM,
+  FAT_POSSUM
 } = RETAILERS;
+
+export const LISSIE_CATCHING_A_TIGER: IVinyl = {
+  musician: MUSICIANS.LISSIE,
+  photo: PHOTOS.ALBUM_ARTWORK.LISSIE_CATCHING_A_TIGER,
+  appleMusicId: "catching-a-tiger/372594767",
+  title: "Catching a Tiger",
+  year: 2010,
+  sides: 2,
+  inches: 12,
+  signed: false,
+  colors: [BLACK],
+  arrived: false,
+  costExcDelivery: 12.99,
+  retailer: FAT_POSSUM
+};
+
+export const TAYLOR_SWIFT_FOLKLORE: IVinyl = {
+  musician: MUSICIANS.TAYLOR_SWIFT,
+  title: "folklore",
+  costExcDelivery: 26.99,
+  retailer: MUSICIAN_WEB_STORE,
+  appleMusicId: "folklore-deluxe-version/1528112358",
+  year: 2020,
+  arrived: false,
+  colors: [COLORS.VINYL.TAYLOR_SWIFT_FOLKLORE],
+  inches: 12,
+  sides: 4,
+  photo: PHOTOS.ALBUM_ARTWORK.TAYLOR_SWIFT_FOLKLORE,
+  signed: false
+};
+
+export const TROYE_SIVAN_IN_A_DREAM: IVinyl = {
+  musician: MUSICIANS.TROYE_SIVAN,
+  title: "In a Dream",
+  photo: PHOTOS.ALBUM_ARTWORK.TROYE_SIVAN_IN_A_DREAM,
+  appleMusicId: "in-a-dream-ep/1523003841",
+  year: 2020,
+  costExcDelivery: 21.99,
+  retailer: MUSICIAN_WEB_STORE,
+  arrived: false,
+  colors: COLORS.VINYL.TROYE_SIVAN_IN_A_DREAM,
+  sides: 2,
+  signed: true,
+  //TODO: verify that it is 10" not 12"
+  inches: 10
+};
+
+export const CHARLI_XCX_HOW_IM_FEELING_NOW: IVinyl = {
+  musician: MUSICIANS.CHARLI_XCX,
+  title: "how i'm feeling now",
+  retailer: MUSICIAN_WEB_STORE,
+  year: 2020,
+  costExcDelivery: 18.99,
+  signed: false,
+  inches: 12,
+  sides: 2,
+  arrived: false,
+  colors: [COLORS.VINYL.CHARLI_XCX_HOW_IM_FEELING_NOW],
+  photo: PHOTOS.ALBUM_ARTWORK.CHARLI_XCX_HOW_IM_FEELING_NOW,
+  appleMusicId: "how-im-feeling-now/1513162098"
+};
 
 export const CARIBOU_OUR_LOVE: IVinyl = {
   musician: MUSICIANS.CARIBOU,
@@ -394,7 +456,7 @@ export const KIM_PETRAS_CLARITY: IVinyl = {
   signed: false,
   inches: 12,
   sides: 2,
-  arrived: true,
+  arrived: false,
   colors: [COLORS.VINYL.PHOTO],
   appleMusicId: "clarity/1469298192"
 };
